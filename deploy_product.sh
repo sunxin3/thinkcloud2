87b4af6e66efe277fb1170f90a0654a4bd1e24f6
Sun Xin <sunxin3@lenovo.com>
2013-11-11 17:35:28 +0800

    add the deploy script for production env

---
+++ deploy_product.sh
@@ -0,0 +1,14 @@
+rm /usr/share/openstack-dashboard -rf
+cp openstack-dashboard/ /usr/share/ -rf
+chown apache:apache /usr/share/openstack-dashboard -R
+
+rm /usr/lib/python2.6/site-packages/horizon -rf
+cp horizon/ /usr/lib/python2.6/site-packages/ -rf
+
+mv /etc/httpd/conf.d/ssl.conf /root/
+
+cp openstack-dashboard/openstack_dashboard/templates/index.html /var/www/html/
+cp openstack-dashboard/openstack_dashboard/templates/css /var/www/html/ -rf
+cp openstack-dashboard/openstack_dashboard/templates/images /var/www/html/ -rf
+
+service httpd restart
